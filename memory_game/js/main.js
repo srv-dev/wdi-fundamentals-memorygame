dd5c6a76860a8311a31d607be46620f5520f3073
Sreevani Pasarlapati <sreevani.pasarla@gmail.com>
2019-11-23 00:57:11 +1100

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,8 @@
+
+var cardOne = "Queen";
+var cardTwo = "King";
+var cardThree = "Queen";
+var cardFour = "King";
+
+console.log("User flipped " + cardOne);
+console.log("User flipepd " + cardFour);
