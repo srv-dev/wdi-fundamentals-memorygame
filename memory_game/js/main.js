2ae939ede552a92341185148b97ec19f979ab8d2
Sreevani Pasarlapati <sreevani.pasarla@gmail.com>
2019-11-23 14:48:10 +1100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,18 @@
 
-var cardOne = "Queen";
-var cardTwo = "King";
-var cardThree = "Queen";
-var cardFour = "King";
+const cards = ['queen','queen','king','king'];
+const cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[1];
 
-console.log("User flipped " + cardOne);
-console.log("User flipepd " + cardFour);
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if(cardsInPlay.length === 2)
+{
+	console.log('User flipped '+ cardsInPlay);
+
+	if(cardsInPlay[0] === cardsInPlay[1])
+		alert('You found a match!');
+	else
+		alert('Sorry, try again!');
+}
