9b8d84451e0181faeab029534a0e8d7964018e15
Sreevani Pasarlapati <sreevani.pasarla@gmail.com>
2019-11-24 15:59:54 +1100

    added DOM functionality

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -25,17 +25,19 @@ const cards = [
 ]; 
 
 function checkForMatch() {
-	if(cardsInPlay[0] === cardsInPlay[1])
-			console.log('You found a match!');
-		else
-			console.log('Sorry, try again!');
-}
-function flipCard(cardId) {
 	
-	console.log("User flipped " + cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
+	if(cardsInPlay[0] === cardsInPlay[1])
+			alert('You found a match!');
+		else
+			alert('Sorry, try again!');
+}
+
+function flipCard() {
+	
+	var cardId = this.getAttribute('data-id');
+	
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src',cards[cardId].cardImage);
 
 	if(cardsInPlay.length === 2)
 	{	
@@ -43,9 +45,17 @@ function flipCard(cardId) {
 	}
 }
 
-flipCard(0);
-flipCard(2);
+function createBoard() {
+	
+	for (var i=0; i<cards.length; i++) {
+		
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id',i);
+		cardElement.addEventListener('click',flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
 
-
-
+createBoard();
 
